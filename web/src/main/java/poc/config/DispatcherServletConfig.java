1c6034e0e293b55a257f6f091d371412031abba8
Johan Sandberg Sjöblom <johan.sandberg.sjoblom@skandia.se>
2021-09-16 16:53:55 +0200

    Statiska resurser på annan än defaultsökväg

--- web/src/main/java/poc/config/DispatcherServletConfig.java
+++ web/src/main/java/poc/config/DispatcherServletConfig.java
@@ -3,18 +3,20 @@ package poc.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @ComponentScan(basePackageClasses = {
 		poc.webmvc.controllers.PackageMarker.class,
 })
-public class DispatcherServletConfig //extends WebMvcConfigurerAdapter
+public class DispatcherServletConfig extends WebMvcConfigurerAdapter
 {
-//	@Override
-//	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-//		super.addResourceHandlers(registry);
-//		registry.addResourceHandler("/img/**")
-//				.addResourceLocations("classpath:static/img/");
-//	}
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+		registry.addResourceHandler("/img/**")
+				.addResourceLocations("classpath:statiskt/img/");
+	}
 }
