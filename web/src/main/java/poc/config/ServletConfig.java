65f03e6df60120703af437a694dccd18d40f0f0b
Johan Sandberg Sjöblom <johan.sandberg.sjoblom@skandia.se>
2021-09-20 10:45:25 +0200

    Flyttar ServletComponentScan till ServletConfig-klassen

--- web/src/main/java/poc/config/ServletConfig.java
+++ web/src/main/java/poc/config/ServletConfig.java
@@ -1,16 +1,18 @@
 package poc.config;
 
 
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import poc.config.stripes.StripesConfig;
-import poc.config.struts.StrutsConfig;
 
 
 @Configuration
+@ServletComponentScan(basePackageClasses = {
+        poc.struts.servlet.PackageMarker.class,
+})
 @Import({
         StripesConfig.class,
-        StrutsConfig.class,
 })
 public class ServletConfig
 {
